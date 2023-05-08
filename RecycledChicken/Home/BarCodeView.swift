edb05e7048d9402973ea11be345e185d14710185
ClaudLin <claud25315488@gmail.com>
2023-05-08 13:46:22 +0800

    no message

---
+++ RecycledChicken/Home/BarCodeView.swift
@@ -0,0 +1,26 @@
+//
+//  BarCodeView.swift
+//  RecycledChicken
+//
+//  Created by 林書郁 on 2023/5/8.
+//
+
+import UIKit
+
+class BarCodeView: UIView, NibOwnerLoadable {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        customInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        customInit()
+    }
+    
+    private func customInit(){
+        loadNibContent()
+    }
+
+}
