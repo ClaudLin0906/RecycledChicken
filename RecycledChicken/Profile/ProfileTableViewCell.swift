2c0255b569a38708ce2328afec466a82243e6bbb
ClaudLin <claud25315488@gmail.com>
2023-05-09 15:00:54 +0800

    no message

---
+++ RecycledChicken/Profile/ProfileTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ProfileTableViewCell.swift
+//  RecycledChicken
+//
+//  Created by 林書郁 on 2023/5/9.
+//
+
+import UIKit
+
+class ProfileTableViewCell: UITableViewCell {
+    
+    static let identifier = "ProfileTableViewCell"
+    
+    @IBOutlet weak var infoTitle:UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- RecycledChicken/Profile/ProfileVC.swift
+++ RecycledChicken/Profile/ProfileVC.swift
@@ -9,26 +9,39 @@ import UIKit
 
 class ProfileVC: CustomVC {
     
+    @IBOutlet weak var profileTableView:UITableView!
+    
+    let profileInfoArr:[String] = ["用戶名稱", "E-mail", "手機號碼", "生日"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "個人賬戶"
         setDefaultNavigationBackBtn2()
-        // Do any additional setup after loading the view.
+        UIInit()
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+    private func UIInit(){
+        profileTableView.setSeparatorLocation()
+        profileTableView.separatorColor = CommonColor.shared.color5
+    }
+
+}
+
+extension ProfileVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        50
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        profileInfoArr.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ProfileTableViewCell.identifier, for: indexPath) as! ProfileTableViewCell
+        cell.infoTitle.text = profileInfoArr[indexPath.row]
+        return cell
+    }
+    
+    
 }
