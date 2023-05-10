5b789c46f31ee87539d4c02982b8a716bbcc9edc
ClaudLin <claud25315488@gmail.com>
2023-05-11 00:11:45 +0800

    no message

---
+++ RecycledChicken/MyTicker/MyTickerTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  MyTickerTableViewCell.swift
+//  RecycledChicken
+//
+//  Created by 林書郁 on 2023/5/11.
+//
+
+import UIKit
+
+class MyTickerTableViewCell: UITableViewCell {
+    
+    static let identifier = "MyTickerTableViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ RecycledChicken/MyTicker/MyTickerVC.swift
@@ -0,0 +1,43 @@
+//
+//  MyTickerVC.swift
+//  RecycledChicken
+//
+//  Created by 林書郁 on 2023/5/11.
+//
+
+import UIKit
+
+class MyTickerVC: CustomVC {
+    
+    @IBOutlet weak var tableView:UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "我的票夾"
+        setDefaultNavigationBackBtn2()
+        UIInit()
+        // Do any additional setup after loading the view.
+    }
+    
+    private func UIInit(){
+        
+    }
+
+}
+
+extension MyTickerVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        200
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CommodityVoucherTableViewCell.identifier, for: indexPath) as! CommodityVoucherTableViewCell
+        return cell
+    }
+    
+}
