d872d4c1a00b2af33d5c6c6273ad45147733231e
ClaudLin <claud25315488@gmail.com>
2023-05-11 21:43:52 +0800

    no message

---
+++ RecycledChicken/PointRule/PointRuleVC.swift
@@ -0,0 +1,24 @@
+//
+//  PointRuleVC.swift
+//  RecycledChicken
+//
+//  Created by 林書郁 on 2023/5/11.
+//
+
+import UIKit
+
+class PointRuleVC: CustomVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "點數機制"
+        setDefaultNavigationBackBtn2()
+        UIInit()
+        // Do any additional setup after loading the view.
+    }
+    
+    private func UIInit(){
+        
+    }
+
+}
