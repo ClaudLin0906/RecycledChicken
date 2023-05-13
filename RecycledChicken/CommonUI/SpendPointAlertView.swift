2fc06f48a21f4de6eec9dd201cdddd92a608b09f
ClaudLin <claud25315488@gmail.com>
2023-05-13 19:04:54 +0800

    no message

---
+++ RecycledChicken/CommonUI/SpendPointAlertView.swift
@@ -0,0 +1,25 @@
+//
+//  SpendPointAlertView.swift
+//  RecycledChicken
+//
+//  Created by 林書郁 on 2023/5/13.
+//
+
+import UIKit
+
+class SpendPointAlertView: UIView, NibOwnerLoadable {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        customInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        customInit()
+    }
+    
+    private func customInit(){
+        loadNibContent()
+    }
+}
