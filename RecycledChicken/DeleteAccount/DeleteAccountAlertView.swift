a88f25a0c294f5ded666792ebacad3cf82ae0389
ClaudLin <claud25315488@gmail.com>
2023-05-15 00:22:03 +0800

    no message

---
+++ RecycledChicken/DeleteAccount/DeleteAccountAlertView.swift
@@ -0,0 +1,26 @@
+//
+//  DeleteAccountAlertView.swift
+//  RecycledChicken
+//
+//  Created by 林書郁 on 2023/5/15.
+//
+
+import UIKit
+
+class DeleteAccountAlertView: UIView, NibOwnerLoadable {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        customInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        customInit()
+    }
+    
+    private func customInit(){
+        loadNibContent()
+    }
+
+}
