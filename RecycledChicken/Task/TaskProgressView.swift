1bf33ed7a7512a7cd200bbe6069fa906986ccfd0
ClaudLin <claud25315488@gmail.com>
2023-05-16 15:15:52 +0800

    no message

---
+++ RecycledChicken/Task/TaskProgressView.swift
@@ -0,0 +1,26 @@
+//
+//  TaskProgressView.swift
+//  RecycledChicken
+//
+//  Created by 林書郁 on 2023/5/16.
+//
+
+import UIKit
+
+class TaskProgressView: UIView, NibOwnerLoadable {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        customInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        customInit()
+    }
+    
+    private func customInit(){
+        loadNibContent()
+    }
+
+}
---
+++ RecycledChicken/Task/TaskTableViewADCell.swift
@@ -0,0 +1,25 @@
+//
+//  TaskTableViewADCell.swift
+//  RecycledChicken
+//
+//  Created by 林書郁 on 2023/5/16.
+//
+
+import UIKit
+
+class TaskTableViewADCell: UITableViewCell {
+    
+    static let identifier = "TaskTableViewADCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
