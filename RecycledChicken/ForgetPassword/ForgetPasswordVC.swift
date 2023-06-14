c5b0726ddba918da853b9a04c9ae16a4e23caac7
ClaudLin <claud25315488@gmail.com>
2023-06-14 16:31:41 +0800

    no message

--- RecycledChicken/ForgetPassword/ForgetPasswordVC.swift
+++ RecycledChicken/ForgetPassword/ForgetPasswordVC.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class ForgetPasswordVC: CustomLoginVC {
+    
+    @IBOutlet weak var phoneTextfield:UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,15 +17,20 @@ class ForgetPasswordVC: CustomLoginVC {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func sendSMS(_ sender:UIButton){
+        var alertMsg = ""
+        let phone = phoneTextfield.text
+        
+        if phone == "" {
+            alertMsg += "電話不能為空"
+        } else if !validateCellPhone(text: phone!) {
+            alertMsg += "電話格式不對"
+        }
+        alertMsg = removeWhitespace(from: alertMsg)
+        guard alertMsg == "" else {
+            showAlert(VC: self, title: nil, message: alertMsg, alertAction: nil)
+            return
+        }
     }
-    */
 
 }
