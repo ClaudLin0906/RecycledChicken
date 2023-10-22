4ebf422672ea959d382bc296104bc57e436a7d5d
ClaudLin <claud25315488@gmail.com>
2023-10-22 16:59:33 +0800

    no message

---
+++ RecycledChickenWidget/RecycledChickenWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  RecycledChickenWidgetBundle.swift
+//  RecycledChickenWidget
+//
+//  Created by 林書郁 on 2023/10/22.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct RecycledChickenWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        RecycledChickenWidget()
+        RecycledChickenWidgetLiveActivity()
+    }
+}
