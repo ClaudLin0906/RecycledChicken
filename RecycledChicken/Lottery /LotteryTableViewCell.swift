4777f61ed4daa4f261f386088d6e9071ca6e29e5
ClaudLin <claud25315488@gmail.com>
2023-10-26 23:07:12 +0800

    no message

--- RecycledChicken/Lottery /LotteryTableViewCell.swift
+++ RecycledChicken/Lottery /LotteryTableViewCell.swift
@@ -35,24 +35,25 @@ class LotteryTableViewCell: UITableViewCell {
     }
     
     func setCell(lotteryInfo:LotteryInfo) {
-        let data = try? Data(contentsOf: URL(string: lotteryInfo.picture)!)
-        let activityStartTimeDate = dateFromString(lotteryInfo.activityStartTime)
-        let activityEndTimeDate = dateFromString(lotteryInfo.activityEndTime)
-        let StartDate = getDates(i: 0, currentDate: activityStartTimeDate!).0
-        let EndDate = getDates(i: 0, currentDate: activityEndTimeDate!).0
-        let lotteryDrawDate = lotteryInfo.lotteryDrawDate
-        DispatchQueue.main.async { [self] in
-            if let data = data, let image = UIImage(data: data) {
-                itemImageView.image = image
+        DispatchQueue(label: "com.geek-is-stupid.queue.configure-cell").async {
+            let data = try? Data(contentsOf: URL(string: lotteryInfo.picture)!)
+            let activityStartTimeDate = dateFromString(lotteryInfo.activityStartTime)
+            let activityEndTimeDate = dateFromString(lotteryInfo.activityEndTime)
+            let StartDate = getDates(i: 0, currentDate: activityStartTimeDate!).0
+            let EndDate = getDates(i: 0, currentDate: activityEndTimeDate!).0
+            let lotteryDrawDate = lotteryInfo.lotteryDrawDate
+            DispatchQueue.main.async { [self] in
+                if let data = data, let image = UIImage(data: data) {
+                    itemImageView.image = image
+                }
+                itemName.text = lotteryInfo.itemName
+                duringTime.text = "活動時間:" + StartDate + "~" + EndDate
+                drawTime.text = "開獎日期:" + lotteryDrawDate
+                drawPeople.text = "參與人數:" + String(lotteryInfo.purchaserCount)
+                duringTime.font = duringTime.font.withSize(11)
+                drawTime.font = duringTime.font.withSize(11)
+                point.text = "\(lotteryInfo.itemPrice)"
             }
-            itemName.text = lotteryInfo.itemName
-            duringTime.text = "活動時間:" + StartDate + "~" + EndDate
-            drawTime.text = "開獎日期:" + lotteryDrawDate
-            drawPeople.text = "參與人數:" + String(lotteryInfo.purchaserCount)
-            duringTime.font = duringTime.font.withSize(11)
-            drawTime.font = duringTime.font.withSize(11)
-            point.text = "\(lotteryInfo.itemPrice)"
         }
     }
-
 }
