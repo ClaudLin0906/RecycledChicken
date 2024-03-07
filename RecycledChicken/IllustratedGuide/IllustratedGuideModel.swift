0f7ba6b3cc7d0accf1d0673108731a1eaeeab3c8
Claud <>
2024-03-07 10:38:25 +0800

    no message

--- RecycledChicken/IllustratedGuide/IllustratedGuideModel.swift
+++ RecycledChicken/IllustratedGuide/IllustratedGuideModel.swift
@@ -10,6 +10,15 @@ import UIKit
 
 var illustratedGuideModelDatas:[IllustratedGuide] =
     [
-        IllustratedGuide(level: 1, guideImage: UIImage(named: "chip-34 1")!, name: "碳員", title: "新手碳教學", iconImage: UIImage(named: "chi-15 1")!, guide: "碳固定是生物將無機碳（二氧化碳） 轉換為有機化合物的過程。"),
-        IllustratedGuide(level: 2, guideImage: UIImage(named: "chip-34 1")!, name: "割草雞", title: "新手碳教學", iconImage: UIImage(named: "chi-15 1")!, guide: "碳固定是生物將無機碳（二氧化碳） 轉換為有機化合物的過程。")
+        IllustratedGuide(level: 1, guideImage: UIImage(named: "chip-34 1")!, name: "碳員", title: "新手碳教學", iconImage: UIImage(named:"level1-icon")!, guide: "碳固定是生物將無機碳（二氧化碳） 轉換為有機化合物的過程。"),
+        IllustratedGuide(level: 2, guideImage: UIImage(named: "Group 174")!, name: "割草雞", title: "堆肥", iconImage: UIImage(named: "chi-15 1")!, guide: "堆肥是被分解和回收的有機物質作為肥料和土壤調理劑。堆肥是有機農業的關鍵成分。"),
+        IllustratedGuide(level: 3, guideImage: UIImage(named: "Group 175")!, name: "潛水雞", title: "水資源", iconImage: UIImage(named: "")!, guide: "全球的水資源只有少於2.5%是淡水，其餘的都是海水。"),
+        IllustratedGuide(level: 4, guideImage: UIImage(named: "Group 176")!, name: "技術雞", title: "技術研發", iconImage: UIImage(named: "")!, guide: "提升回收再製純化技術"),
+        IllustratedGuide(level: 5, guideImage: UIImage(named: "Group 177")!, name: "挖土雞", title: "土壤", iconImage: UIImage(named: "")!, guide: "土壤若遭受污染，不僅土壤品質惡化、根著其上的作物，食用作物的生物亦受波及，甚至影響到地下水源的品質，而威脅到飲用水的安全。"),
+        IllustratedGuide(level: 6, guideImage: UIImage(named: "Group 178")!, name: "紳士雞", title: "紡織纖維材料", iconImage: UIImage(named: "")!, guide: "紡織廠致力於寶特瓶回收紗線的開發，讓寶特瓶進入再生循環，用行動支持減塑愛地球。"),
+        IllustratedGuide(level: 7, guideImage: UIImage(named: "")!, name: "發電雞", title: "綠電", iconImage: UIImage(named: "")!, guide: "綠電即再生能源，是指某種發電過程中，產生的二氧化碳排放量為零，或近乎零，對生態、氣候造成的影響較小。"),
+        IllustratedGuide(level: 8, guideImage: UIImage(named: "")!, name: "冒險雞", title: "大眾運輸", iconImage: UIImage(named: "")!, guide: "完善綠色運輸規劃、減少私有運具是更低碳的交通方式。"),
+        IllustratedGuide(level: 9, guideImage: UIImage(named: "")!, name: "花雕雞", title: "親民代表", iconImage: UIImage(named: "")!, guide: "城市美化，循環經濟基礎建設"),
+        IllustratedGuide(level: 10, guideImage: UIImage(named: "")!, name: "碳長", title: "碳匯", iconImage: UIImage(named: "")!, guide: "碳匯（carbon sink）是儲存二氧化碳的天然或人工「倉庫」")
+        
     ]
