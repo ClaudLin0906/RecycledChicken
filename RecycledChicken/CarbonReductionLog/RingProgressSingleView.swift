df37ba09a31d3c71aee11fd4623f4fe13c93871e
Claud <>
2024-03-31 14:52:39 +0800

    no message

--- RecycledChicken/CarbonReductionLog/RingProgressSingleView.swift
+++ RecycledChicken/CarbonReductionLog/RingProgressSingleView.swift
@@ -12,6 +12,16 @@ class RingProgressSingleView: UIView {
 
     private let ring = RingProgressView()
     
+    private var stackView:UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .fillEqually
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
     private lazy var targetLabel = labelInit()
     
     private lazy var countLabel = labelInit()
@@ -51,6 +61,7 @@ class RingProgressSingleView: UIView {
     
     private func setup() {
         addSubview(ring)
+        addSubview(stackView)
 //        addSubview(targetLabel)
 //        addSubview(slashLabel)
 //        addSubview(totalLabel)
@@ -73,32 +84,36 @@ class RingProgressSingleView: UIView {
         ring.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
         ring.widthAnchor.constraint(equalTo: widthAnchor, constant: 0).isActive = true
         ring.heightAnchor.constraint(equalTo: heightAnchor, constant: 0).isActive = true
-//        targetLabel.text = "TARGET"
-//        targetLabel.centerXAnchor.constraint(equalTo: ring.centerXAnchor).isActive = true
-//        targetLabel.centerYAnchor.constraint(equalTo: ring.centerYAnchor,constant: -20).isActive = true
-//        targetLabel.widthAnchor.constraint(equalTo: ring.widthAnchor, multiplier: 0.7).isActive = true
-//        targetLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
-//        targetLabel.font = UIFont(name: "GenJyuuGothic-Heavy", size: 15)
-//        
-//        let totalColor = #colorLiteral(red: 0.6571641564, green: 0.6571640372, blue: 0.6571640372, alpha: 1)
-//        
-//        slashLabel.text = "/"
-//        slashLabel.textColor = totalColor
-//        slashLabel.font = UIFont(name: "GenJyuuGothic-Medium", size: 10)
-//        slashLabel.centerXAnchor.constraint(equalTo: targetLabel.centerXAnchor).isActive = true
-//        slashLabel.centerYAnchor.constraint(equalTo: ring.centerYAnchor,constant: 20).isActive = true
-//        
-//        totalLabel.textColor = totalColor
-//        totalLabel.font = UIFont(name: "GenJyuuGothic-Medium", size: 10)
-//        totalLabel.centerYAnchor.constraint(equalTo: slashLabel.centerYAnchor).isActive = true
-//        totalLabel.leadingAnchor.constraint(equalTo: slashLabel.trailingAnchor, constant: 5).isActive = true
-//        
-//        countLabel.font = UIFont(name: "jf-openhuninn-2.0", size: 25)
-//        countLabel.bottomAnchor.constraint(equalTo: slashLabel.bottomAnchor).isActive = true
-//        countLabel.trailingAnchor.constraint(equalTo: slashLabel.leadingAnchor, constant: -5).isActive = true
         
-//        ring.layer.shadowOffset = CGSize(width: 1, height: 1)
-//        ring.layer.shadowOpacity = 0.2
+        stackView.centerXAnchor.constraint(equalTo: ring.centerXAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: ring.centerYAnchor).isActive = true
+        stackView.widthAnchor.constraint(equalTo: ring.widthAnchor, multiplier: 0.9).isActive = true
+        stackView.heightAnchor.constraint(equalTo: ring.heightAnchor, multiplier: 0.9).isActive = true
+
+        targetLabel.text = "TARGET"
+        targetLabel.font = UIFont(name: "GenJyuuGothic-Heavy", size: 15)
+        
+        let countView = UIView()
+        countView.backgroundColor = .clear
+        countView.addSubview(slashLabel)
+        countView.addSubview(totalLabel)
+        countView.addSubview(countLabel)
+        let totalColor = #colorLiteral(red: 0.6571641564, green: 0.6571640372, blue: 0.6571640372, alpha: 1)
+        slashLabel.text = "/"
+        slashLabel.textColor = totalColor
+        slashLabel.font = UIFont(name: "GenJyuuGothic-Medium", size: 10)
+        
+        totalLabel.textColor = totalColor
+        totalLabel.font = UIFont(name: "GenJyuuGothic-Medium", size: 10)
+        totalLabel.centerYAnchor.constraint(equalTo: slashLabel.centerYAnchor).isActive = true
+        totalLabel.leadingAnchor.constraint(equalTo: slashLabel.trailingAnchor, constant: 5).isActive = true
+        
+        countLabel.font = UIFont(name: "jf-openhuninn-2.0", size: 25)
+        countLabel.bottomAnchor.constraint(equalTo: slashLabel.bottomAnchor).isActive = true
+        countLabel.trailingAnchor.constraint(equalTo: slashLabel.leadingAnchor, constant: -5).isActive = true
+        
+        stackView.addArrangedSubview(targetLabel)
+        stackView.addArrangedSubview(countView)
     }
     
     func setCount(_ count:Double, _ total:Double, _ startColor:UIColor, _ endColor:UIColor) {
