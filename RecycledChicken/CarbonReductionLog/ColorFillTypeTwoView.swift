cf52bbb0674746a855ed948acbf6a4f2a72c0da9
Claud <>
2024-04-04 14:10:09 +0800

    no message

---
+++ RecycledChicken/CarbonReductionLog/ColorFillTypeTwoView.swift
@@ -0,0 +1,25 @@
+//
+//  ColorFillTypeTwoView.swift
+//  RecycledChicken
+//
+//  Created by Claud on 2024/4/4.
+//
+
+import UIKit
+
+class ColorFillTypeTwoView: UIView, NibOwnerLoadable {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        customInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        customInit()
+    }
+    
+    private func customInit(){
+        loadNibContent()
+    }
+}
