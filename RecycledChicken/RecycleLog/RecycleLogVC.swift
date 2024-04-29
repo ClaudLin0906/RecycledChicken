0086398ac05d660422b6b6b148811af9b924ffd6
Claud <>
2024-04-29 12:12:11 +0800

    no message

--- RecycledChicken/RecycleLog/RecycleLogVC.swift
+++ RecycledChicken/RecycleLog/RecycleLogVC.swift
@@ -43,7 +43,7 @@ class RecycleLogVC: CustomVC {
         monthBtn.newImageView.tintColor = CommonColor.shared.color1
         setupAmountDropDown()
         if getLanguage() == .english {
-            monthBtnWidth.constant = 130
+            monthBtnWidth.constant = 150
         }
     }
     
