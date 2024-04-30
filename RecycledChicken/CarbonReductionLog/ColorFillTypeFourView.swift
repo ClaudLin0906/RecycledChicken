895a7e1079068efe4202b5e35d143c4f28529b0a
Claud <>
2024-04-30 11:29:58 +0800

    no message

--- RecycledChicken/CarbonReductionLog/ColorFillTypeFourView.swift
+++ RecycledChicken/CarbonReductionLog/ColorFillTypeFourView.swift
@@ -43,6 +43,26 @@ class ColorFillTypeFourView: UIView, NibOwnerLoadable {
         addGestureRecognizer(tap)
     }
     
+    private func getKeyName(_ imageViewTag:Int) -> String {
+        var result = ""
+        let target = imageViews.first(where: {$0.tag == imageViewTag})
+        switch target?.tag {
+        case 0:
+            result = UserDefaultKey.shared.bigFillRightChickenOfColorFillTypeFourView
+        case 1:
+            result = UserDefaultKey.shared.bigFillLeftChickenOfColorFillTypeFourView
+        case 2:
+            result = UserDefaultKey.shared.bigFillLeftChickenOfColorFillTypeFourView
+        case 3:
+            result = UserDefaultKey.shared.twoFillRightChickenOfColorFillTypeFourView
+        case 4:
+            result = UserDefaultKey.shared.threeFillRightChickenOfColorFillTypeFourView
+        default :
+            break
+        }
+        return result
+    }
+    
     @objc private func tapImageView(_ tapGesture:UITapGestureRecognizer) {
         if let imageView = tapGesture.view as? UIImageView {
             delegate?.tapImage(imageView)
@@ -52,4 +72,5 @@ class ColorFillTypeFourView: UIView, NibOwnerLoadable {
     @objc private func tapView(_ tapGesture:UITapGestureRecognizer) {
         delegate?.tapBackground(self)
     }
+    
 }
