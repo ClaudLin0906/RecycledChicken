0c46c2899d2ca83d7b3a6eb47b6107c69628a986
ClaudLin <claud.lin@focaltech-sensors.com>
2024-05-23 14:34:15 +0800

    no message

--- RecycledChicken/MainTabBarController.swift
+++ RecycledChicken/MainTabBarController.swift
@@ -11,7 +11,7 @@ class MainTabBarController: UITabBarController {
     
     let tabbarTitle:[String] =
     [
-        "p".localized,
+        "point".localized,
         "store".localized,
         "home".localized,
 //        "AR",
