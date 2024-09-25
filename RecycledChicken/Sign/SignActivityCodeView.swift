df2f07cb417ade0c1634f4a6ae95091a1494b7fc
Claud <>
2024-09-25 12:04:43 +0800

    邀請碼 活動碼 新增

--- RecycledChicken/Sign/SignActivityCodeView.swift
+++ RecycledChicken/Sign/SignActivityCodeView.swift
@@ -15,6 +15,8 @@ class SignActivityCodeView: UIView, NibOwnerLoadable {
     
     @IBOutlet weak var activityCodeTextField:UITextField!
     
+    @IBOutlet weak var friendActivityCodeTextField:UITextField!
+    
     @IBOutlet weak var errorMSGLabel:CustomLabel!
         
     var delegate:SignActivityCodeViewDelegate?
@@ -48,6 +50,52 @@ class SignActivityCodeView: UIView, NibOwnerLoadable {
         errorMSGLabel.isHidden = false
     }
     
+    private func enterActivityCodeAction(_ compeletion: @escaping(Bool, String)->()) {
+        NetworkManager.shared.requestWithJSONBody(urlString: APIUrl.domainName + APIUrl.enterActivityCode) { data, statusCode, errorMSG in
+            guard let data = data, statusCode == 200 else {
+                var errorMSG = "發生不明錯誤"
+                if let statusCode = statusCode {
+                    switch statusCode {
+                        case 400:
+                            errorMSG = "活動碼不能為空"
+                        case 404:
+                            errorMSG = "活動碼不正確"
+                        case 500:
+                            errorMSG = "活動碼處理失敗"
+                        default:
+                            break
+                    }
+                }
+                compeletion(false, errorMSG)
+                return
+            }
+            compeletion(true, "")
+        }
+    }
+    
+    private func enterInviteCodeAction(_ compeletion: @escaping(Bool, String)->()) {
+        NetworkManager.shared.requestWithJSONBody(urlString: APIUrl.domainName + APIUrl.enterInviteCode) { data, statusCode, errorMSG in
+            guard let data = data, statusCode == 200 else {
+                var errorMSG = "發生不明錯誤"
+                if let statusCode = statusCode {
+                    switch statusCode {
+                        case 400:
+                            errorMSG = "邀請碼為空"
+                        case 404:
+                            errorMSG = "邀請碼錯誤"
+                        case 403:
+                            errorMSG = "邀請碼已經被輸入"
+                        default:
+                            break
+                    }
+                }
+                compeletion(false, errorMSG)
+                return
+            }
+            compeletion(true, "")
+        }
+    }
+
     @IBAction func confirm(_ sender:CustomButton) {
         var alertMsg = ""
         let activityCode = activityCodeTextField.text
@@ -59,21 +107,31 @@ class SignActivityCodeView: UIView, NibOwnerLoadable {
             showErrorMSG(alertMsg)
             return
         }
-        NetworkManager.shared.requestWithJSONBody(urlString: APIUrl.domainName + APIUrl.enterActivityCode) { data, statusCode, errorMSG in
-            guard let data = data, statusCode == 200 else {
-                self.showErrorMSG(errorMSG ?? "")
-                return
+        let group = DispatchGroup()
+        var activityCodeSuccess = false
+        var inviteCodeSuccess = friendActivityCodeTextField.text == "" ? true : false
+        var errorMSG = ""
+        if !inviteCodeSuccess {
+            group.enter()
+            enterInviteCodeAction { (result, message) in
+                inviteCodeSuccess = result
+                errorMSG += message
+                group.leave()
             }
-            let ApiResult = try? JSONDecoder().decode(ApiResult.self, from: data)
-            if let status = ApiResult?.status {
-                switch status {
-                case .success:
-                    self.delegate?.comfirmInvitationCode(finishAction: {
-                        self.removeFromSuperview()
-                    })
-                case .failure:
-                    self.showErrorMSG(ApiResult?.message ?? "")
-                }
+        }
+        group.enter()
+        enterActivityCodeAction { (result, message) in
+            activityCodeSuccess = result
+            errorMSG += message
+            group.leave()
+        }
+        group.notify(queue: .main) {
+            if activityCodeSuccess && inviteCodeSuccess {
+                self.delegate?.comfirmInvitationCode(finishAction: {
+                    self.removeFromSuperview()
+                })
+            } else {
+                self.showErrorMSG(errorMSG)
             }
         }
     }
