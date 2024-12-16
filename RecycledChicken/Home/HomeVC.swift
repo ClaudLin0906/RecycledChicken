50e45fac849efda31c52df8f7a57858a51d2cdb2
林書郁 <claud25315488@gmail.com>
2024-12-16 16:01:49 +0800

    no message

--- RecycledChicken/Home/HomeVC.swift
+++ RecycledChicken/Home/HomeVC.swift
@@ -260,7 +260,7 @@ class HomeVC: CustomRootVC {
         batteryItemView.setInfo(.battery)
         papperCubItemView.setInfo(.papperCub)
         aluminumCanItemView.setInfo(.aluminumCan)
-        mallCollectionViewFlowLayout.itemSize = CGSize(width: mallCollectionView.frame.size.width / 3 - 5, height: 100)
+        mallCollectionViewFlowLayout.itemSize = CGSize(width: mallCollectionView.frame.size.width / 3 - 10, height: UIScreen.main.bounds.size.height / 8)
         mallCollectionViewFlowLayout.estimatedItemSize = .zero
         mallCollectionViewFlowLayout.minimumInteritemSpacing = 0
         mallCollectionViewFlowLayout.minimumLineSpacing = 0
